9c2e58e765cc93cb88ab320208d1c30737f21a0c
vanBach76 <ludvig.backlund@gmail.com>
2023-12-25 22:10:25 +0100

    Some fix

--- src/components/CompanySelector.js
+++ src/components/CompanySelector.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Modal from './Modal';
 import CompanyForm from './CompanyForm';
+import { useMutation } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const ADD_COMPANY = gql`
+  mutation {
+    addCompany(companyInput: $companyInput)
+  }
+`;
 
 function CompanySelector({ companyList, selectCompany }) {
 
@@ -18,7 +26,7 @@ function CompanySelector({ companyList, selectCompany }) {
                 {selectableCompanies}
                 <button className="btn btn-dark" data-toggle="modal" data-target="#modalCenter">Add Company</button>
             </div>
-            <Modal Content={ CompanyForm } title="Add Company" />
+            <Modal Content={ CompanyForm } title="Add Company" onClick=""/>
         </div>
     );
 }
